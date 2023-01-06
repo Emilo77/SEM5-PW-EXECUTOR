81b10c4cfdc8534280dde5a1d5044317e83b874b
Kamil <kb417522@students.mimuw.edu.pl>
2023-01-06 01:43:51 +0100

    Remove unnecessary class parameters

--- Task.cpp
+++ Task.cpp
@@ -1,5 +1,2 @@
-//
-// Created by emilo77 on 25.12.22.
-//
-
 #include "Task.h"
+
--- Task.h
+++ Task.h
@@ -7,37 +7,11 @@
 
 using std::string;
 
-enum TaskType {
-    RUN,
-    OUT,
-    ERR,
-    KILL,
-};
-
 class Task {
 private:
-    TaskType taskType;
-
     //todo args
-
 public:
-    Task(TaskType taskType):
-        taskType(taskType) {}
 
-    TaskType getType() { return taskType; }
-
-    void execute() {
-        switch (taskType) {
-        case RUN:
-            break;
-        case OUT:
-            break;
-        case ERR:
-            break;
-        case KILL:
-            break;
-        }
-    }
 };
 
 #endif // EXECUTOR_TASK_H
